6ee3a9f0292f54225ed57c98a42d6f3a562e89cf
Rakesh Kumar <acousticrakesh0827@gmail.com>
2023-01-16 22:04:18 +0530

    Quick Commit

--- src/Project_Files/ConfirmChoice.java
+++ src/Project_Files/ConfirmChoice.java
@@ -4,7 +4,6 @@ import javax.swing.*;
 
 public class ConfirmChoice extends JFrame {
     static JFrame frame;
-
     public static boolean confirmOptionYesNo(){
         frame = new JFrame();
         return JOptionPane.showConfirmDialog(frame, "Press Yes / No.", "Confirm Your Choice !", JOptionPane.YES_NO_OPTION)==JOptionPane.YES_NO_OPTION;
